5f18cb68a71f6e7316ca8be9a9e5eadac2011b56
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-06-10 20:40:07 +0300

    corrections

--- src/main/java/org/example/MovieManager.java
+++ src/main/java/org/example/MovieManager.java
@@ -13,25 +13,30 @@ public class MovieManager {
         this.count = count;
     }
 
-    public void addMovie(MovieList item){
-        MovieList[] tmp = new MovieList[movieListArr.length+1];
-        for (int i = 0; i < movieListArr.length ; i++) {
+    public void addMovie(MovieList item) {
+        MovieList[] tmp = new MovieList[movieListArr.length + 1];
+        for (int i = 0; i < movieListArr.length; i++) {
             tmp[i] = movieListArr[i];
         }
-        tmp[tmp.length-1] = item;
+        tmp[tmp.length - 1] = item;
         movieListArr = tmp;
     }
 
-    public MovieList[] findAll(){
+    public MovieList[] findAll() {
         return movieListArr;
     }
 
-    public MovieList[] findLast(){
+    public MovieList[] findLast() {
+        if (movieListArr.length > count) {
+            this.count = count;
+        } else {
+            count = movieListArr.length;
+        }
         MovieList[] tmp = new MovieList[count];
         int lastMovie = movieListArr.length;
 
-        for (int i = 0; i < movieListArr.length ; i++) {
-            tmp[i] = movieListArr[lastMovie-1-i];
+        for (int i = 0; i < movieListArr.length; i++) {
+            tmp[i] = movieListArr[lastMovie - 1 - i];
         }
         return tmp;
     }
--- src/test/java/org/example/MovieManagerTest.java
+++ src/test/java/org/example/MovieManagerTest.java
@@ -7,16 +7,16 @@ import java.util.Arrays;
 
 class MovieManagerTest {
 
-    MovieList movie1 = new MovieList(1, "боевик","бладшот", 111);
-    MovieList movie2 = new MovieList(2, "мультфильм","вперед", 222);
-    MovieList movie3 = new MovieList(3, "комедия","отель белгарад", 333);
-    MovieList movie4 = new MovieList(4, "боевик","джентльмены", 444);
-    MovieList movie5 = new MovieList(5, "ужасы","человек невидимка", 555);
-    MovieList movie6 = new MovieList(6, "мультфильм","тролли мировой тур", 666);
-    MovieList movie7 = new MovieList(7, "комедия","номер один", 777);
+    MovieList movie1 = new MovieList(1, "боевик", "бладшот", 111);
+    MovieList movie2 = new MovieList(2, "мультфильм", "вперед", 222);
+    MovieList movie3 = new MovieList(3, "комедия", "отель белгарад", 333);
+    MovieList movie4 = new MovieList(4, "боевик", "джентльмены", 444);
+    MovieList movie5 = new MovieList(5, "ужасы", "человек невидимка", 555);
+    MovieList movie6 = new MovieList(6, "мультфильм", "тролли мировой тур", 666);
+    MovieList movie7 = new MovieList(7, "комедия", "номер один", 777);
 
     @Test
-    public void addMovie(){
+    public void addMovie() {
         MovieManager movie = new MovieManager();
         movie.addMovie(movie1);
         movie.addMovie(movie2);
@@ -34,7 +34,7 @@ class MovieManagerTest {
     }
 
     @Test
-    public void addOneMovie(){
+    public void addOneMovie() {
         MovieManager movie = new MovieManager();
         movie.addMovie(movie1);
 
@@ -46,7 +46,7 @@ class MovieManagerTest {
     }
 
     @Test
-    public void addEmptyMovie(){
+    public void addEmptyMovie() {
         MovieManager movie = new MovieManager();
 
         MovieList[] expected = {};
@@ -57,7 +57,7 @@ class MovieManagerTest {
     }
 
     @Test
-    public void findLast(){
+    public void findLast() {
         MovieManager movie = new MovieManager(7);
         movie.addMovie(movie1);
         movie.addMovie(movie2);
@@ -65,11 +65,10 @@ class MovieManagerTest {
         movie.addMovie(movie4);
         movie.addMovie(movie5);
         movie.addMovie(movie6);
-        movie.addMovie(movie7);
 
 
         MovieList[] actual = movie.findLast();
-        MovieList[] expected = {movie7, movie6, movie5, movie4, movie3, movie2, movie1};
+        MovieList[] expected = {movie6, movie5, movie4, movie3, movie2, movie1};
         Assertions.assertArrayEquals(expected, actual);
 
         System.out.println(Arrays.deepToString(actual));
@@ -77,7 +76,7 @@ class MovieManagerTest {
     }
 
     @Test
-    public void findLastFive(){
+    public void findLastFive() {
         MovieManager movie = new MovieManager();
         movie.addMovie(movie1);
         movie.addMovie(movie2);
@@ -92,6 +91,44 @@ class MovieManagerTest {
 
         System.out.println(Arrays.deepToString(actual));
 
+    }
+
+    @Test
+    public void findLastOneMovie() {
+        MovieManager movie = new MovieManager();
+        movie.addMovie(movie1);
+
+        MovieList[] actual = movie.findLast();
+        MovieList[] expected = {movie1};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastOneMovieParam() {
+        MovieManager movie = new MovieManager(1);
+        movie.addMovie(movie1);
+
+        MovieList[] actual = movie.findLast();
+        MovieList[] expected = {movie1};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastEmpty() {
+        MovieManager movie = new MovieManager();
+
+        MovieList[] actual = movie.findLast();
+        MovieList[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastEmptyParam() {
+        MovieManager movie = new MovieManager(10);
+
+        MovieList[] actual = movie.findLast();
+        MovieList[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
     }
 
 }
